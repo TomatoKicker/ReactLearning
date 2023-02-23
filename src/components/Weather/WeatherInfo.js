c48e774069ae2d8ee64be8f1e90535602f6429fc
TomatoKicker <phi465@live.de>
2023-02-23 22:57:04 +0100

    api

--- src/components/Weather/WeatherInfo.js
+++ src/components/Weather/WeatherInfo.js
@@ -1,5 +1,5 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faWind, faWater } from "@fortawesome/free-solid-svg-icons";
 
 function WeatherInfo() {
--- src/components/Weather/WeatherSearchBox.js
+++ src/components/Weather/WeatherSearchBox.js
@@ -2,15 +2,40 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLocationDot, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
+
+function test(){
+    const inputField = document.querySelector('input')
+    if(inputField.value == ''){
+        alert('test');
+        return;
+    }
+    const country = inputField.value;
+    const apiKey = '8124c75248bcbb397a816b9bed4e89ce'
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=${apiKey}`
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(
+            json =>{
+                if (json.cod === '404'){
+                    alert('404') 
+                }else{
+                alert('nice')
+                }
+            } 
+        );
+}
+
 function WeatherSearchBox() {
     return (
         <div className="weather-searchbox">
-            <FontAwesomeIcon icon={faLocationDot} />
+           <FontAwesomeIcon icon={faLocationDot} size="2x" />
             <input type="text" placeholder="Enter your country"></input>
-            <button>
+            <button onClick={test}>
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
             </button>
         </div>
     )
 }
+
 export default WeatherSearchBox
